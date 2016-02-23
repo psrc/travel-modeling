58a6b39043b4dad606e8c536f7159e1681edc7a0
Brice Nichols <bricenichols@gmail.com>
2016-02-22 17:05:38 -0800

    script to compute upslope from emme network

---
+++ slope/slope_config.py
@@ -0,0 +1,7 @@
+geodb = r'D:\slope\slope921.gdb'
+slope_csv = 'slope_output.csv'
+bike_attr_table = r'D:\slope\slope921.gdb\tblBikeAttributes'
+
+# Input network location and desired output feature class name
+in_fc = r"D:\slope\slope921.gdb\edges_0"
+output_fc = r'\edges_0_test6'
