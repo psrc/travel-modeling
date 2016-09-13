234fbc73298678ff57c646ef22b5833b514fcfd8
bricegnichols <bricenichols@gmail.com>
2016-09-13 14:22:41 -0700

    compare alogit f12 output for different estimation sets

---
+++ estimation/estimation.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+def load_f12(working_dir, fname, suffix=None):
+    """
+    Convert F12 output to dataframe, calcualte t-stat
+    """
+    df = pd.read_csv(working_dir + r'\\' + fname, delim_whitespace=True, skiprows=1)
+    
+    # Assume a fixed format for conversion
+    df = df.ix[1:,0:5]
+    cols = ['coefficient','constrained','value','standard error']
+    if suffix != None:
+        cols = [col + suffix for col in cols]
+    cols.insert(0,'id')
+    df.columns = cols
+
+    # drop the covariance matrix bit, marked with -1 
+    df = df.ix[:df[df['id'] == "-1"].index.tolist()[0]-1,:]
+    
+    # Calculate a t-statistic
+    df['standard error'] = df['standard error'].astype('float')
+    df['t-stat'] = abs(df['value']/df['standard error'])
+    
+    return df
+
+def compare_df(df1,df2,suffixes):
+    """
+    Compare two F12 based dataframes
+    """
+        
+    df = pd.merge(df1, df2, on='coefficient', suffixes=suffixes)
+    df.index = df['coefficient']
+    
+    # set resolution
+    figsize=(len(df),len(df)*.5)
+    
+    field = 'value'
+    print df[[field+suffix for suffix in suffixes]].plot(kind='barh',figsize=figsize, title='coefficient value',
+                                                         width=2, alpha=0.6)
+    
+    field = 't-stat'
+    print [field+suffix for suffix in suffixes]
+    print df[[field+suffix for suffix in suffixes]].astype('float').plot(kind='barh', figsize=figsize, title='t-stat', 
+                                                                         width=2, alpha=0.6)
+
+    return df
